0d55ac5705a8c479b47a6691b4a974207cf02f90
JonKraber <174822049+chill-gamer-dev@users.noreply.github.com>
2025-01-19 15:57:27 -0600

    Improved performance of obj file parsing by utilizing std::string_view. read_obj_file() now runs roughly 2x faster.

--- GraphicsDemo/ObjLoader.cpp
+++ GraphicsDemo/ObjLoader.cpp
@@ -4,12 +4,14 @@ module;
 
 #include <array>
 #include <fstream>
+#include <ranges>
 #include <unordered_map>
 
 module ObjLoader;
 
 //import <array>;
 //import <fstream>; // not working?
+//import <ranges>;
 //import <unordered_map>;
 
 namespace
@@ -36,6 +38,31 @@ struct std::hash<ObjVertex>
 
 namespace ObjLoader
 {
+	auto get_tokens(std::string_view str, std::string_view delim)
+	{
+		return std::views::split(str, delim)
+			| std::views::filter([](auto token) { return std::ranges::distance(token) > 0; })
+			| std::views::transform([](auto token) { return std::string_view(&*token.begin(), std::ranges::distance(token)); });
+	}
+
+	float to_float(std::string_view str)
+	{
+		float result;
+		auto [_, ec] = std::from_chars(str.data(), str.data() + str.size(), result);
+		if (ec != std::errc{})
+			throw std::runtime_error("Failed to parse float from obj file.");
+		return result;
+	}
+
+	unsigned int to_uint(std::string_view str)
+	{
+		unsigned int result;
+		auto [_, ec] = std::from_chars(str.data(), str.data() + str.size(), result);
+		if (ec != std::errc{})
+			throw std::runtime_error("Failed to parse unsigned int from obj file.");
+		return result;
+	}
+
 	bool read_obj_file(std::filesystem::path filepath,
 		std::vector<std::array<float, 3>> & positions,
 		std::vector<std::array<float, 3>> & normals,
@@ -46,43 +73,51 @@ namespace ObjLoader
 		if (!obj_file.is_open())
 			return false;
 
-		while (obj_file.good())
+		std::string line;
+		while (std::getline(obj_file, line))
 		{
-			std::string token;
-			obj_file >> token;
+			auto tokens = get_tokens(line, " ");
+			if (tokens.empty())
+				continue;
 
-			if (token == "#") // comment
-			{
-				obj_file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			}
-			else if (token == "v") // vertex position
-			{
-				auto & pos = positions.emplace_back();
-				obj_file >> pos[0] >> pos[1] >> pos[2];
-			}
-			else if (token == "vn") // vertex normal
-			{
-				auto & norm = normals.emplace_back();
-				obj_file >> norm[0] >> norm[1] >> norm[2];
-			}
-			else if (token == "f") // face
-			{
-				std::string line;
-				std::getline(obj_file, line);
-				std::istringstream ss_line(line);
-
-				ObjFaceVerts verts;
-				for (std::string vert_token; std::getline(ss_line, vert_token, ' ');)
+			auto token_iter = tokens.begin();
+			auto next_token = [&token_iter, &tokens]() -> std::string_view
 				{
-					if (vert_token.empty())
-						continue;
+					if (token_iter == tokens.end())
+						throw std::runtime_error("Unexpected number of tokens when reading obj file.");
+					return *token_iter++;
+				};
 
-					char * context = nullptr;
-					unsigned int pos_i = std::atoi(strtok_s(vert_token.data(), "/", &context));
-					unsigned int norm_i = std::atoi(strtok_s(nullptr, "/", &context));
-					verts.push_back(ObjVertex{ pos_i, norm_i });
+			std::string_view element_type = next_token();
+			if (element_type == "v") // vertex position
+			{
+				std::array<float, 3> & pos = positions.emplace_back();
+				pos[0] = to_float(next_token());
+				pos[1] = to_float(next_token());
+				pos[2] = to_float(next_token());
+			}
+			else if (element_type == "vn") // vertex normal
+			{
+				std::array<float, 3> & norm = normals.emplace_back();
+				norm[0] = to_float(next_token());
+				norm[1] = to_float(next_token());
+				norm[2] = to_float(next_token());
+			}
+			else if (element_type == "f") // face
+			{
+				ObjFaceVerts & verts = face_verts.emplace_back();
+				while (token_iter != tokens.end())
+				{
+					std::string_view vert_str = next_token();
+
+					auto vert_tokens = get_tokens(vert_str, "/");
+					if (std::ranges::distance(vert_tokens) != 2)
+						throw std::runtime_error("Unexpected number of vertex tokens when reading obj file.");
+
+					unsigned int pos_i = to_uint(*vert_tokens.begin());
+					unsigned int norm_i = to_uint(*std::next(vert_tokens.begin()));
+					verts.emplace_back(pos_i, norm_i);
 				}
-				face_verts.push_back(std::move(verts));
 			}
 		}
 
